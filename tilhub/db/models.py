b956ae55bde2161b77152ae32fd05c921805760c
Jason Costello <jjacostello@gmail.com>
2020-06-20 17:34:11 +0100

    New User dataclass

--- tilhub/db/models.py
+++ tilhub/db/models.py
@@ -16,3 +16,18 @@ class TodayILearned(db.Model):
     added = Column(DateTime, default=func.now())
     user = Column(String)
     til = Column(String)
+
+
+@dataclass
+class User(db.Model):
+
+    id: int
+    username: str
+    email: str
+
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(100), nullable=False, unique=True)
+    email = Column(String(100), nullable=False)
+    password = Column(String(200), nullable=False)
+    date_created = Column(DateTime, default=func.now())
