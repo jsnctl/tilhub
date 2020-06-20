d3c7754a8413d96ded77c73d130d0ea7a1e4c0fa
Jason Costello <jjacostello@gmail.com>
2020-06-20 17:34:38 +0100

    Get that honkin yaml import abstracted to new singleton, add AuthEndpoint

--- tilhub/api.py
+++ tilhub/api.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from extensions import db, api
-import yaml
 from endpoints.til_endpoint import TILEndpoint
+from endpoints.auth_endpoint import AuthEndpoint
+from config import Config
 
-CREDENTIALS = yaml.load(open("./credentials.yaml", "rb"),
-                        Loader=yaml.FullLoader)
+config = Config().config
 DATABASE_URL = "postgres://{0}:{1}@localhost:5432/tilhub".format(
-    CREDENTIALS['database']['username'],
-    CREDENTIALS['database']['password']
+    config['database']['username'],
+    config['database']['password']
 )
 
 
@@ -17,6 +17,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+    api.add_resource(AuthEndpoint, '/auth')
     api.add_resource(TILEndpoint, '/til/<id>')
 
     api.init_app(app)
