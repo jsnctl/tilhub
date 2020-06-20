3c9833268ff4d418319612f6f1ab0da749914b2e
Jason Costello <jjacostello@gmail.com>
2020-06-21 00:06:12 +0100

    Add controller and endpoint for basic addition of TILs

--- tilhub/controllers/til_controller.py
+++ tilhub/controllers/til_controller.py
@@ -1,8 +1,28 @@
 from db.models import TodayILearned
+import logging
+from extensions import db
+from config import Config
+from sqlalchemy.exc import SQLAlchemyError
+
+logger = logging.getLogger(__name__)
+config = Config().config
 
 
 class TILController:
 
-    def get_til_by_id(self, id):
-        result = TodayILearned.query.filter_by(id=id).first()
+    @staticmethod
+    def get_til_by_id(til_id):
+        result = TodayILearned.query.filter_by(id=til_id).first()
         return result
+
+    @staticmethod
+    def add_til(user, til):
+
+        try:
+            til = TodayILearned(user=user, til=til)
+            db.session.add(til)
+            db.session.commit()
+            return til
+        except SQLAlchemyError:
+            logger.error('Problem creating TIL')
+            return None
--- tilhub/endpoints/til_endpoint.py
+++ tilhub/endpoints/til_endpoint.py
@@ -1,12 +1,32 @@
 from flask import jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from controllers.til_controller import TILController
 
 controller = TILController()
 
+parser = reqparse.RequestParser()
+parser.add_argument("user")
+parser.add_argument("til")
 
-class TILEndpoint(Resource):
 
-    def get(self, id):
-        til = controller.get_til_by_id(id)
+class TILGetEndpoint(Resource):
+
+    @staticmethod
+    def get(til_id):
+        til = controller.get_til_by_id(til_id)
         return jsonify(til)
+
+
+class TILPostEndpoint(Resource):
+
+    @staticmethod
+    def post():
+        args = parser.parse_args()
+        result = controller.add_til(user=args['user'],
+                                    til=args['til'])
+
+        if result is None:
+            return jsonify("Error creating TIL")
+
+        return jsonify(result)
+
