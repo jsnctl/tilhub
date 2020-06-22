a008dbe746e87f5b648a4807a3e1132b7d9c179b
Jason Costello <jjacostello@gmail.com>
2020-06-22 22:52:17 +0100

    Name refactoring

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tilhub',
-    version='0.0.01',
+    name='tilmine',
+    version='0.0.1',
     description='TIL, and I do not want to forget',
 
     author='J. J. A. Costello',
 
-    packages=find_packages(where='tilhub'),
-    package_dir={'': 'tilhub'}
+    packages=find_packages(where='tilmine'),
+    package_dir={'tilmine': 'tilmine'},
+    package_data={'tilmine': '*.yaml'}
 )
