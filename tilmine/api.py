523485350847c6a35bf859bf9e70c4631cb4dfa5
Jason Costello <jjacostello@gmail.com>
2020-06-23 21:57:26 +0100

    Search endpoint now uses POST

--- tilmine/api.py
+++ tilmine/api.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from extensions import db, api
-from endpoints.til_endpoint import (TILGetEndpoint, TILPostEndpoint, TILGetByTagEndpoint)
+from endpoints.til_endpoint import (TILGetEndpoint, TILPostEndpoint, TILSearchByTagEndpoint)
 from endpoints.auth_endpoint import AuthEndpoint
 from config import Config
 
@@ -19,7 +19,7 @@ def create_app():
 
     api.add_resource(AuthEndpoint, '/auth')
     api.add_resource(TILGetEndpoint, '/til/<til_id>')
-    api.add_resource(TILGetByTagEndpoint, '/til/tags/<tag>')
+    api.add_resource(TILSearchByTagEndpoint, '/til/search/tags')
     api.add_resource(TILPostEndpoint, '/til')
 
     api.init_app(app)
--- tilmine/endpoints/til_endpoint.py
+++ tilmine/endpoints/til_endpoint.py
@@ -12,23 +12,26 @@ parser.add_argument("tags", action='append')
 
 class TILGetEndpoint(Resource):
 
-    @staticmethod
-    def get(til_id):
+    def get(self, til_id):
         til = controller.get_til_by_id(til_id)
         return jsonify(til)
 
 
-class TILGetByTagEndpoint(Resource):
+class TILSearchByTagEndpoint(Resource):
 
-    def get(self, tag):
-        tils = controller.get_tils_by_tag(tag)
+    def post(self):
+        args = parser.parse_args()
+        tags = args['tags']
+
+        tils = []
+        for tag in tags:
+            tils.append(controller.get_tils_by_tag(tag))
         return jsonify(tils)
 
 
 class TILPostEndpoint(Resource):
 
-    @staticmethod
-    def post():
+    def post(self):
         args = parser.parse_args()
         result = controller.add_til(user=args['user'],
                                     til=args['til'],
