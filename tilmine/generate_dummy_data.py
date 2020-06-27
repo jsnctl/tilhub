d59f9ec3f696108846f63c9c9d984345f8f51919
Jason Costello <jjacostello@gmail.com>
2020-06-27 22:16:11 +0100

    More realistic data generation for testing

--- tilmine/generate_dummy_data.py
+++ tilmine/generate_dummy_data.py
@@ -1,9 +1,6 @@
 import requests
-import numpy as np
 
-
-users = ['jsnctl', 'topo', 'chappie', 'test_user', 'dhh']
-tils = [
+tils = {"jsnctl": [
     {"til": "Docker for Mac default binds to no particular address 0.0.0.0, not to loopback 127.0.0.1",
      "tags": ["docker", "mac", "python"]},
     {"til": "PySpark might need spark.executorEnv.PYTHONHASHSEED set to a random int, as 3.3+/Spark 2+ doesnt seem to "
@@ -16,19 +13,29 @@ tils = [
      "tags": ["aws", "hadoop"]},
     {"til": "For aws-cli on a Mac, dont even bother with the suggested pip-based install. brew install awscli works "
             "seamlesslyy",
-     "tags": ["aws", "hadoop", "mac"]},
-]
+     "tags": ["aws", "hadoop", "mac"]}
+],
+    "test_user": [
+        {"til": "Keras will often need a refresh of the env variables LD_LIBRARY_PATH and CUDA_HOME",
+         "tags": ["keras", "python"]},
+        {"til": "Docker/zsh tab completion in gist (https://gist.github.com/ro31337/b2c33ad0b90636e9e3bb76fb4fb76907)",
+         "tags": ["docker", "zsh", "command line"]},
+        {"til": "Kill a list of PIDS associated with an lsof/open files: lsof -i :port | awk...",
+         "tags": ["command line"]},
+        {"til": "Build caching and Dockerfiles...",
+         "tags": ["docker"]}
+    ]
+}
 
-for _ in range(100):
+for user in tils.keys():
 
-    til = np.random.choice(tils, 1)[0]
-    user = np.random.choice(users, 1)[0]
+    user_tils = tils[user]
 
-    response = requests.post("http://localhost:5000/til",
-                  json={'user': user,
-                        'til': til['til'],
-                        'tags': til['tags']},
-                  headers={'content-type': 'application/json'})
+    for til in user_tils:
+        response = requests.post("http://localhost:5000/til",
+                                 json={'user': user,
+                                       'til': til['til'],
+                                       'tags': til['tags']},
+                                 headers={'content-type': 'application/json'})
 
     print(response.text)
-
