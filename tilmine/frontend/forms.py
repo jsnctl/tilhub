532df5cb13ac943989bdb9dd481d61907c4400fd
Jason Costello <jjacostello@gmail.com>
2020-07-05 21:15:25 +0100

    [WIP] Login form assets

--- tilmine/frontend/forms.py
+++ tilmine/frontend/forms.py
@@ -1,7 +1,12 @@
-from wtforms import Form, StringField
+from wtforms import Form, StringField, PasswordField
 
 
 class TILSearch(Form):
     search = StringField('', render_kw={
         'style': 'font-size:30pt;'
     })
+
+
+class Login(Form):
+    username = StringField('')
+    password = PasswordField('')
