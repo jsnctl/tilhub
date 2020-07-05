354eeb25eae7276e7f181bb0696b8981e5187523
Jason Costello <jjacostello@gmail.com>
2020-07-05 21:16:16 +0100

    Remove redundant app.py code after factory pattern

--- tilmine/frontend/app.py
+++ tilmine/frontend/app.py
@@ -1,10 +1,6 @@
 import logging
-from flask import Flask, render_template, request
-from tilmine.frontend.forms import TILSearch, Login
+from flask import Flask
 from tilmine.config import Config
-import requests
-from tilmine.frontend.usecase.search import tag_search
-from tilmine.frontend.extensions import login_manager
 from tilmine.frontend.routes import blueprint
 
 
@@ -16,33 +12,9 @@ PORTS = Config.config['ports']
 
 def create_app():
     app = Flask(__name__)
-    # login_manager.init_app(app)
     app.register_blueprint(blueprint)
 
     return app
-
-
-
-# @app.route("/", methods=['GET', 'POST'])
-# def home():
-#
-#     login = Login(request.form)
-#     search = TILSearch(request.form)
-#     tils = []
-#     if request.method == 'POST':
-#         tils = tag_search(search)
-#
-#     return render_template("tils.html", posts=tils, form=search)
-#
-#
-# def search_results(search):
-#     query = search.data['search']
-#
-#     tils = requests.post("http://{0}:{1}/til/search/tags".format(SERVER, PORTS['api']),
-#                          json={"tags": "{0}".format(query)},
-#                          headers={'content-type': 'application/json'}).json()
-#
-#     return tils
 
 
 if __name__ == '__main__':
